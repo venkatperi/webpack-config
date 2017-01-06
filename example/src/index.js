297f48973570d7e773a98e14127517b5ec44c5c8
Biavati, Alessandro <Alessandro.Biavati@ca.com>
2017-01-05 16:34:05 -0800

    chore: added copyright information to test files

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,10 @@
-// @flow
-
 /**
  * Copyright (c) 2016 CA. All rights reserved.
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
+
+// @flow
 
 import React from 'react';
 import { render } from 'react-dom';
